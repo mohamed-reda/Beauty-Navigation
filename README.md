e74a0761c53dd7fc11131aa778038bc92e2a97e1
Mohamed Reda Tech <mohamed.reda.007007@gmail.com>
2019-07-13 18:36:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Beauty-Navigation
+ Abeauty ui Navigation
---
+++ flutter_a/lib/main.dart
@@ -0,0 +1,130 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_a/page_dragger.dart';
+import 'package:flutter_a/page_reveal.dart';
+import 'package:flutter_a/pager_indicator.dart';
+import 'package:flutter_a/pages.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Material Page Reveal',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+
+  StreamController<SlideUpdate> slideUpdateStream;
+  AnimatedPageDragger animatedPageDragger;
+
+  int activeIndex = 0;
+  int nextPageIndex = 0;
+  SlideDirection slideDirection = SlideDirection.none;
+  double slidePercent = 0.0;
+
+  _MyHomePageState() {
+    slideUpdateStream = new StreamController<SlideUpdate>();
+
+    slideUpdateStream.stream.listen((SlideUpdate event) {
+      setState(() {
+        if (event.updateType == UpdateType.dragging) {
+          print('Sliding ${event.direction} at ${event.slidePercent}');
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+
+          if (slideDirection == SlideDirection.leftToRight) {
+            nextPageIndex = activeIndex - 1;
+          } else if (slideDirection == SlideDirection.rightToLeft) {
+            nextPageIndex = activeIndex + 1;
+          } else {
+            nextPageIndex = activeIndex;
+          }
+        } else if (event.updateType == UpdateType.doneDragging) {
+          print('Done dragging.');
+          if (slidePercent > 0.5) {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.open,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this,
+            );
+          } else {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.close,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this,
+            );
+
+            nextPageIndex = activeIndex;
+          }
+
+          animatedPageDragger.run();
+        } else if (event.updateType == UpdateType.animating) {
+          print('Sliding ${event.direction} at ${event.slidePercent}');
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+        } else if (event.updateType == UpdateType.doneAnimating) {
+          print('Done animating. Next page index: $nextPageIndex');
+          activeIndex = nextPageIndex;
+
+          slideDirection = SlideDirection.none;
+          slidePercent = 0.0;
+
+          animatedPageDragger.dispose();
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(
+        children: [
+          new Page(
+            viewModel: pages[activeIndex],
+            percentVisible: 1.0,
+          ),
+          new PageReveal(
+            revealPercent: slidePercent,
+            child: new Page(
+              viewModel: pages[nextPageIndex],
+              percentVisible: slidePercent,
+            ),
+          ),
+          new PagerIndicator(
+            viewModel: new PagerIndicatorViewModel(
+              pages,
+              activeIndex,
+              slideDirection,
+              slidePercent,
+            ),
+          ),
+          new PageDragger(
+            canDragLeftToRight: activeIndex > 0,
+            canDragRightToLeft: activeIndex < pages.length - 1,
+            slideUpdateStream: this.slideUpdateStream,
+          ),
+        ],
+      ),
+    );
+  }
+}
